125a134fbb3b6df0446188b9231fa45353dee58f
Jimmy Leung <jimmy.rl19@hotmail.com>
2019-02-16 20:35:21 -0200

    reusable header component

--- albums/index.js
+++ albums/index.js
@@ -4,8 +4,8 @@ import { name as appName } from './app.json';
 import Header from './src/components/header';
 
 const App = () => (
-    <Header />
-)
+    <Header text="Albums" />
+);
 
 // At least one component must be registered
 // Bootstrap our application called appName registering our App Component
