6acd02a205c9733fa2505ef8111af0baf832895e
Jimmy Leung <jimmy.rl19@hotmail.com>
2019-02-28 23:03:42 -0300

    firebase install and configuration

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { Header } from './components/common'
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 
 class App extends Component {
+
+  componentWillMount() {
+    // We initialize the firebase before our component mount
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBg9nKczULLNtMDx6lmr8O8SGSW7ISPfyI',
+      authDomain: 'learning-rn-auth.firebaseapp.com',
+      databaseURL: 'https://learning-rn-auth.firebaseio.com',
+      projectId: 'learning-rn-auth',
+      storageBucket: 'learning-rn-auth.appspot.com',
+      messagingSenderId: '69981354833'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Header text="Authentication" />
+        <Header text='Authentication' />
         <Text>Welcome to React Native!</Text>
       </View >
     );
