8b9efbe8c4ab1b2d27de2aef5ea2829fc15d505f
Jimmy Leung <jimmy.rl19@hotmail.com>
2019-03-12 21:59:37 -0300

    basic redux configuration

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
-const App = () => <View />;
+/* Provider */
+// The provider is the component that 'translates' the data within the store 
+// to be used by the react application (communication)
+
+/* Store */
+// The store is the component that holds our application data (states)
+
+const App = () => (
+    <Provider store={createStore(reducers)}>
+        <View />
+    </Provider>
+);
 
 export default App;
---
+++ tech_stack/src/reducers/index.js
@@ -0,0 +1,7 @@
+// The combineReducers allow us to easily combine multiple pieces of state
+// and make them play nice together
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    libraries: () => []
+})
